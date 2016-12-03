c14ddceb540f3eb4124693d95f3f8bc32303b856
zharley18 <zackharley@gmail.com>
2016-12-03 10:05:52 -0500

    Add Mongoose integration

--- src/server.js
+++ src/server.js
@@ -2,19 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const morgan = require('morgan');
 const routes = require('./routes');
+const cors = require('./controllers/cors');
+const db = require('./db');
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors);
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes.index);
 

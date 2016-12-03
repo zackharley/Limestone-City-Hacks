0a5fa625f574b7833c279a379ac6fa3fa311c25e
zharley18 <zackharley@gmail.com>
2016-12-03 09:45:34 -0500

    Add handlebars

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +10,11 @@ const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public'));
 
 app.use(routes.index);
 
